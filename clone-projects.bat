449faa38350d2a2943c163eb456288d412021960
m27 <m27@topdanmark.dk>
2016-12-29 16:44:04 +0800

    updates

---
+++ clone-projects.bat
@@ -0,0 +1,14 @@
+cd ..
+git clone https://github.com/great-design-and-systems/security-service.git
+git clone https://github.com/great-design-and-systems/gds-logger.git
+git clone https://github.com/great-design-and-systems/category-service.git
+git clone https://github.com/great-design-and-systems/faculty-service.git
+git clone https://github.com/great-design-and-systems/student-service.git
+git clone https://github.com/great-design-and-systems/item-service.git
+git clone https://github.com/great-design-and-systems/gds-ticket-service.git
+git clone https://github.com/great-design-and-systems/gds-event-service.git
+git clone https://github.com/great-design-and-systems/gds-event-processor.git
+git clone https://github.com/great-design-and-systems/import-service.git
+git clone https://github.com/great-design-and-systems/file-service.git
+git clone https://github.com/great-design-and-systems/gds-ms-api.git
+cd gds-env
---
+++ run-envs.bat
@@ -0,0 +1,38 @@
+cd ..
+cd security-service
+start cmd /k nf start dev
+cd ..
+cd gds-logger
+start cmd /k nf start dev
+cd ..
+cd student-service
+start cmd /k nf start app
+cd .. 
+cd faculty-service
+start cmd /k nf start app
+cd ..
+cd file-service
+start cmd /k nf start web
+cd ..
+cd import-service
+start cmd /k nf start web
+cd ..
+cd item-service
+start cmd /k nf start dev
+cd ..
+cd category-service
+start cmd /k nf start dev
+cd ..
+cd gds-event-service
+start cmd /k nf start dev
+cd ..
+cd gds-event-processor
+start cmd /k nf start dev
+cd ..
+cd gds-ticket-service
+start cmd /k nf start dev
+cd ..
+cd gds-ms-api
+start cmd /k nf start dev
+cd ..
+cd gds-env
