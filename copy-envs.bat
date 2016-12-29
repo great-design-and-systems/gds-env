b575fe2d4a3e3ded7ce5d82717ea17b8130f012a
m27 <m27@topdanmark.dk>
2016-12-29 15:38:58 +0800

    updates

---
+++ copy-envs.bat
@@ -0,0 +1,12 @@
+copy security-service\.env  ..\security-service\.env
+copy gds-logger\.env  ..\gds-logger\.env
+copy category-service\.env  ..\category-service\.env
+copy faculty-service\.env  ..\faculty-service\.env
+copy student-service\.env  ..\student-service\.env
+copy item-service\.env  ..\item-service\.env
+copy gds-ticket-service\.env  ..\gds-ticket-service\.env
+copy gds-event-service\.env  ..\gds-event-service\.env
+copy gds-event-processor\.env  ..\gds-event-processor\.env
+copy import-service\.env  ..\import-service\.env
+copy file-service\.env  ..\file-service\.env
+copy gds-ms-api\.env  ..\gds-ms-api\.env
