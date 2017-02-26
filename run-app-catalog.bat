81f5869325e891535ff249a280d730670f397bef
rickzx98 <rickzx98@gmail.com>
2017-02-26 13:08:58 +0800

    added minimum service

--- run-app-catalog.bat
+++ run-app-catalog.bat
@@ -9,24 +9,9 @@ git pull
 start "security-service" cmd forever start /k nf start dev
 ping 127.0.0.1 -n 6 > nul
 cd ..
-cd student-service
-git pull
-start "student-service" cmd forever start /k nf start app
-ping 127.0.0.1 -n 6 > nul
-cd .. 
-cd faculty-service
-git pull
-start "faculty-service" cmd forever start /k nf start app
-ping 127.0.0.1 -n 6 > nul
-cd ..
 cd file-service
 git pull
 start "file-service" cmd forever start /k nf start web
-ping 127.0.0.1 -n 6 > nul
-cd ..
-cd import-service
-git pull
-start "import-service" cmd forever start /k nf start web
 ping 127.0.0.1 -n 6 > nul
 cd ..
 cd gds-event-service
@@ -39,24 +24,9 @@ git pull
 start "gds-event-processor" cmd forever start /k nf start dev
 ping 127.0.0.1 -n 6 > nul
 cd ..
-cd gds-ticket-service
-git pull
-start "gds-ticket-service" cmd forever start /k nf start dev
-ping 127.0.0.1 -n 6 > nul
-cd ..
 cd user-service
 git pull
 start "user-service" cmd forever start /k nf start web
-ping 127.0.0.1 -n 6 > nul
-cd ..
-cd time-service
-git pull
-start "time-service" cmd forever start /k nf start web
-ping 127.0.0.1 -n 6 > nul
-cd ..
-cd school-config-service
-git pull
-start "school-config-service" cmd forever start /k nf start web
 ping 127.0.0.1 -n 6 > nul
 cd ..
 cd cataloguing-service
